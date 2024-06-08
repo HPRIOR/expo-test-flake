0cbef6164dd5e8770634ce053a5224d26fe8fa07
Harry Prior <harjp@pm.me>
2024-06-08 12:15:40 +0100

    init

---
+++ devshell.nix
@@ -0,0 +1,37 @@
+{ pkgs }:
+
+with pkgs;
+
+# Configure your development environment.
+#
+# Documentation: https://github.com/numtide/devshell
+devshell.mkShell {
+  name = "android-project";
+  motd = ''
+    Entered the Android app development environment.
+  '';
+  env = [
+    {
+      name = "ANDROID_HOME";
+      value = "${android-sdk}/share/android-sdk";
+    }
+    {
+      name = "ANDROID_SDK_ROOT";
+      value = "${android-sdk}/share/android-sdk";
+    }
+    {
+      name = "JAVA_HOME";
+      value = jdk.home;
+    }
+    {
+        name = "ANDROID_AVD_HOME";
+        value = "/home/harryp/.config/.android/avd/";
+    }
+  ];
+  packages = [
+    android-studio
+    android-sdk
+    gradle
+    jdk
+  ];
+}
